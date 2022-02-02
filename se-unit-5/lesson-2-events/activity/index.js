0930453bb94b590298f301128e23e8e353b3b2f5
Erwins Saget <erwinssaget@gmail.com>
2022-02-02 10:34:16 -0500

    events stuff

---
+++ se-unit-5/lesson-2-events/activity/index.js
@@ -0,0 +1,23 @@
+console.log(`
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                   EVENTS
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+`)
+
+// listening for when our initial document is loaded
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+
+// adding focus/blur events
+
+// querying for elements
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+// adding event listener (click)
+
+// adding key events (key)
+// inspecting an event object
+
+
+// event propogation (what happens when we click nested elements)
+
+// default actions (link, form)
